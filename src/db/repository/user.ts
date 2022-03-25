4fb3949af78d02d42c6b0fdcaf3ae750cf83c366
Deepthi Pottipally <dpottipa@students.kennesaw.edu>
2022-03-25 19:45:38 -0400

    save user details

--- src/db/repository/user.ts
+++ src/db/repository/user.ts
@@ -1,6 +1,6 @@
 import db from "..";
-import { doc, getDoc, setDoc  } from 'firebase/firestore';
-import { UserData } from 'types';
+import { doc, getDoc, setDoc, updateDoc, arrayUnion  } from 'firebase/firestore';
+import { UserData, UserSaveFormData } from 'types';
 const COLLECTION_NAME = "users";
 export const getLoggedInUser = async (user: { uid: string; firstName: string; email: string; photoURL: any; lastName: string; displayname: string; age: number}): Promise<UserData> => {
     const docRef = doc(db, COLLECTION_NAME, user.uid);
@@ -29,4 +29,21 @@ export const getLoggedInUser = async (user: { uid: string; firstName: string; em
         return newDocSnap.data() as UserData;
     }
     
+}
+export const getLoginUser = async (uid: string): Promise<UserData> => {
+    const docRef = doc(db, COLLECTION_NAME, uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        return docSnap.data() as UserData;
+    } else {
+        alert('user does not exist!');
+        return null;
+    }
+}
+export const addUserInterest = async (uid: string, interest: string) => {
+    const docRef = doc(db, COLLECTION_NAME, uid);
+    await updateDoc(docRef, {
+        interests: arrayUnion(interest)
+    });
 }
